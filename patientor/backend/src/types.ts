f6c215e198e24ae29c9a1e639036aef8b9f6cdfb
ZennithYattare <luis.espera.0758@gmail.com>
2024-10-01 01:51:58 +0800

    done 9.11: Patientor backend, step4

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -1,5 +1,18 @@
+// NOTE: Diagnosis
 export interface DiagnosisEntry {
 	code: string;
 	name: string;
 	latin?: string;
 }
+
+// NOTE: Patient
+export interface PatientEntry {
+	id: string;
+	name: string;
+	dateOfBirth: string;
+	ssn: string;
+	gender: string;
+	occupation: string;
+}
+
+export type NonSensitivePatient = Omit<PatientEntry, 'ssn'>;
