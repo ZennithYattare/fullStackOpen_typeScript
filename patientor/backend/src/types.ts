ccaea6518f22c52972016088c7dea2fcc002e179
ZennithYattare <luis.espera.0758@gmail.com>
2024-10-11 22:18:40 +0800

    done Exercise 9.21: Patientor, step1

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -16,10 +16,15 @@ export enum Gender {
 	Other = "other",
 }
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface Entry {
+
+}
+
 export type NewPatientEntry = z.infer<typeof NewPatientEntrySchema>;
 
 export interface PatientEntry extends NewPatientEntry {
 	id: string;
 };
 
-export type NonSensitivePatient = Omit<PatientEntry, "ssn">;
+export type NonSensitivePatient = Omit<PatientEntry, "ssn" | "entries">;
--- patientor/backend/src/utils.ts
+++ patientor/backend/src/utils.ts
@@ -2,12 +2,16 @@ import { z } from "zod";
 
 import { NewPatientEntry, Gender } from "./types";
 
+// Define the Entry schema
+export const EntrySchema = z.object({});
+
 export const NewPatientEntrySchema = z.object({
 	name: z.string(),
-	dateOfBirth: z.string().date(),
 	ssn: z.string(),
-	gender: z.nativeEnum(Gender),
 	occupation: z.string(),
+	gender: z.nativeEnum(Gender),
+	dateOfBirth: z.string().date(),
+	entries: z.array(EntrySchema).optional().default([]),
 });
 
 export const toNewPatientEntry = (object: unknown): NewPatientEntry => {
