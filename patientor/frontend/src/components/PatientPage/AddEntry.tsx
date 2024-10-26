b3766100bc6543a3a3ea8cb0231004f852704be3
ZennithYattare <luis.espera.0758@gmail.com>
2024-10-26 08:32:19 +0800

    done 9.29: Patientor, step 9

--- patientor/frontend/src/components/PatientPage/AddEntry.tsx
+++ patientor/frontend/src/components/PatientPage/AddEntry.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
+import { RadioGroup, FormControlLabel, Radio } from "@mui/material";
+
 import { PatientEntry } from "../../types";
 import HealthCheck from "../EntryForm/HealthCheck";
 import Hospital from "../EntryForm/Hospital";
+import OccupationalHC from "../EntryForm/OccupationalHC";
+import React from "react";
 
 interface AddEntryProps {
 	setEntries: React.Dispatch<React.SetStateAction<PatientEntry["entries"]>>;
@@ -8,12 +13,132 @@ interface AddEntryProps {
 }
 
 const AddEntry = (props: AddEntryProps) => {
-	return (
-		<>
-			<HealthCheck setEntries={props.setEntries} setAddEntryModal={props.setAddEntryModal} />
-			<Hospital setEntries={props.setEntries} setAddEntryModal={props.setAddEntryModal} />
-		</>
-	);
+	const [entryType, setEntryType] = useState<string>("HealthCheck");
+
+	switch (entryType) {
+		case "HealthCheck":
+			return (
+				<>
+					<RadioGroup
+						row
+						aria-labelledby="row-radio-buttons-group-label"
+						name="row-radio-buttons-group"
+						defaultValue={entryType}
+					>
+						<FormControlLabel
+							value="HealthCheck"
+							control={<Radio />}
+							label="Health Check"
+							onChange={(e) =>
+								setEntryType((e.target as HTMLInputElement).value)
+							}
+						/>
+						<FormControlLabel
+							value="Hospital"
+							control={<Radio />}
+							label="Hospital"
+							onChange={(e) =>
+								setEntryType((e.target as HTMLInputElement).value)
+							}
+						/>
+						<FormControlLabel
+							value="OccupationalHealthcare"
+							control={<Radio />}
+							label="Occupational Healthcare"
+							onChange={(e) =>
+								setEntryType((e.target as HTMLInputElement).value)
+							}
+						/>
+					</RadioGroup>
+					<HealthCheck
+						setEntries={props.setEntries}
+						setAddEntryModal={props.setAddEntryModal}
+					/>
+				</>
+			);
+		case "Hospital":
+			return (
+				<>
+					<RadioGroup
+						row
+						aria-labelledby="row-radio-buttons-group-label"
+						name="row-radio-buttons-group"
+						defaultValue={entryType}
+					>
+						<FormControlLabel
+							value="HealthCheck"
+							control={<Radio />}
+							label="Health Check"
+							onChange={(e) =>
+								setEntryType((e.target as HTMLInputElement).value)
+							}
+						/>
+						<FormControlLabel
+							value="Hospital"
+							control={<Radio />}
+							label="Hospital"
+							onChange={(e) =>
+								setEntryType((e.target as HTMLInputElement).value)
+							}
+						/>
+						<FormControlLabel
+							value="OccupationalHealthcare"
+							control={<Radio />}
+							label="Occupational Healthcare"
+							onChange={(e) =>
+								setEntryType((e.target as HTMLInputElement).value)
+							}
+						/>
+					</RadioGroup>
+					<Hospital
+						setEntries={props.setEntries}
+						setAddEntryModal={props.setAddEntryModal}
+					/>
+				</>
+			);
+		case "OccupationalHealthcare":
+			return (
+				<>
+					<RadioGroup
+						row
+						aria-labelledby="row-radio-buttons-group-label"
+						name="row-radio-buttons-group"
+						defaultValue={entryType}
+					>
+						<FormControlLabel
+							value="HealthCheck"
+							control={<Radio />}
+							label="Health Check"
+							onChange={(e) =>
+								setEntryType((e.target as HTMLInputElement).value)
+							}
+						/>
+						<FormControlLabel
+							value="Hospital"
+							control={<Radio />}
+							label="Hospital"
+							onChange={(e) =>
+								setEntryType((e.target as HTMLInputElement).value)
+							}
+						/>
+						<FormControlLabel
+							value="OccupationalHealthcare"
+							control={<Radio />}
+							label="Occupational Healthcare"
+							onChange={(e) =>
+								setEntryType((e.target as HTMLInputElement).value)
+							}
+						/>
+					</RadioGroup>
+					<OccupationalHC
+						setEntries={props.setEntries}
+						setAddEntryModal={props.setAddEntryModal}
+					/>
+				</>
+			);
+		default:
+			return null;
+	}
 };
 
 export default AddEntry;
